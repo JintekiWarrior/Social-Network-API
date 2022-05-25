0f9a78490d31ed0bc32d661928fac7df9b29001f
Moe Yassine <moeyassine1993@gmail.com>
2022-05-25 18:15:43 -0400

    Created the user model

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose")
+
+const UserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+)
+
+UserSchema.virtual("friendsCount").get(function () {
+  return this.friends.length
+})
+
+const User = model("User", UserSchema)
+
+module.exports = User
