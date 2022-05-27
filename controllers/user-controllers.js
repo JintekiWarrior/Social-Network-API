9ff246623f714048fef930453e920aa8de4ddb0e
JintekiWarrior <jintekiwarrior@gmail.com>
2022-05-27 10:41:08 -0400

    Add query to thought-controllers   1. getById - Add populate method to User Index query   1. populates the thought   2. removes version number - Set route for getById

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,22 +1,48 @@
-const { User } = require("../models")
+const { User } = require("../models");
 
 const userController = {
   // Index Users
   indexUsers(req, res) {
     User.find({})
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err)
-        res.status(400),json(err)
+      .populate({
+        path: "thoughts",
+        select: "-__v",
       })
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400), json(err);
+      });
   },
 
   // Create a User
   createUser({ body }, res) {
     User.create(body)
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => res.json(err))
-  }
-}
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
 
-module.exports = userController
+  // Show User
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.userId })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user was found with that id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json;
+      });
+  },
+};
+
+module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,9 +3,13 @@ const router = require("express").Router();
 const {
   indexUsers,
   createUser,
+  getUserById,
 } = require("../../controllers/user-controllers");
 
 // api/users
 router.route("/").get(indexUsers).post(createUser);
+
+// api/users/:userId
+router.route("/:userId").get(getUserById);
 
 module.exports = router;
